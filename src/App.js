81d70f2481912e9183ef7d0a5419f902d12fa790
Tohirul Islam <tohirul.islam164@gmail.com>
2022-10-11 16:09:33 +0600

    Project Started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/layout/Header/Header";
 
 function App() {
     return (
         <div className="App">
-            <h1>Hi</h1>
+            <Header />
         </div>
     );
 }
---
+++ src/components/layout/TopNav/TopNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TopNav.css";
+
+const TopNav = () => {
+    const icons = [];
+    return (
+        <div>
+            <div className="social-icons"></div>
+        </div>
+    );
+};
+
+export default TopNav;
